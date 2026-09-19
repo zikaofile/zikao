e65ad42ff23cefa996adfbea3fd9b37ba6309f40
zikaofile <zikaofile@163.com>
2026-09-20 00:12:06 +0800

    add submod.sh

--- submod.sh
+++ submod.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-git submodule add https://github.com/zikaofile/00023.git 00023
-git submodule add https://github.com/zikaofile/02324.git 02324
-git submodule add https://github.com/zikaofile/06627.git 06627
-git submodule add https://github.com/zikaofile/10315.git 10315
-git submodule add https://github.com/zikaofile/13000.git 13000
-git submodule add https://github.com/zikaofile/13003.git 13003
-git submodule add https://github.com/zikaofile/13005.git 13005
-git submodule add https://github.com/zikaofile/13007.git 13007
-git submodule add https://github.com/zikaofile/13009.git 13009
-git submodule add https://github.com/zikaofile/13013.git 13013
-git submodule add https://github.com/zikaofile/13015.git 13015
-git submodule add https://github.com/zikaofile/13017.git 13017
-git submodule add https://github.com/zikaofile/13180.git 13180
-git submodule add https://github.com/zikaofile/14349.git 14349
-git submodule add https://github.com/zikaofile/14899.git 14899
-git submodule add https://github.com/zikaofile/15040.git 15040
-git submodule add https://github.com/zikaofile/15043.git 15043
-git submodule add https://github.com/zikaofile/15044.git 15044
+git submodule add https://github.com/zikaofile/00023.git 00023高等数学                            
+git submodule add https://github.com/zikaofile/02324.git 02324离散数学                            
+git submodule add https://github.com/zikaofile/06627.git 06627网页制作与网站建设                  
+git submodule add https://github.com/zikaofile/10315.git 10315计算机系统原理                      
+git submodule add https://github.com/zikaofile/13000.git 13000英语                                
+git submodule add https://github.com/zikaofile/13003.git 13003数据结构与算法                      
+git submodule add https://github.com/zikaofile/13005.git 13005软件工程                            
+git submodule add https://github.com/zikaofile/13007.git 13007编译原理                            
+git submodule add https://github.com/zikaofile/13009.git 13009数据库原理与技术                    
+git submodule add https://github.com/zikaofile/13013.git 13013高级语言程序设计                    
+git submodule add https://github.com/zikaofile/13015.git 13015计算机系统原理                      
+git submodule add https://github.com/zikaofile/13017.git 13017计算机网络与信息安全                
+git submodule add https://github.com/zikaofile/13180.git 13180操作系统                            
+git submodule add https://github.com/zikaofile/14349.git 14349网络应用与系统集成                  
+git submodule add https://github.com/zikaofile/14899.git 14899大数据技术基础实践                  
+git submodule add https://github.com/zikaofile/15040.git 15040习近平新时代中国特色社会主义思想概论
+git submodule add https://github.com/zikaofile/15043.git 15043中国近代史纲要                      
+git submodule add https://github.com/zikaofile/15044.git 15044马克思主义基本原理                  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
